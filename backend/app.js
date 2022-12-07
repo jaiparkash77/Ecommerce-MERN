abc67d7838b1434830a0c116f35da545ecb0ba09
Jai Parkash <parkashj05@gmail.com>
2022-12-07 17:59:27 +0530

    Backend User & Password Authentication

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const cookieParser = require("cookie-parser");
+
+const errorMiddleware = require("./middleware/error");
 
 const app = express();
 
 app.use(express.json());
-
-const errorMiddleware = require("./middleware/error");
+app.use(cookieParser());
 
 //Route imports
 const product = require("./routes/productRoute");
+const user = require("./routes/userRoute");
+
 app.use("/api/v1",product);
+app.use("/api/v1",user);
+
 
 // Middleware for Error
 app.use(errorMiddleware);
